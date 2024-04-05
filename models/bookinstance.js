4b79141d2f691e7130c29ebb02bb2139103aa2cc
muha <1student.muhammed1@gmail.com>
2024-04-05 18:16:27 +0300

    feat: insertion forms added

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -28,6 +28,10 @@ BookInstanceSchema.virtual("url").get(function () {
     return `/catalog/bookinstance/${this._id}`;
 });
 
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+    return DateTime.fromJSDate(this.due_back).toISODate(); // format 'YYYY-MM-DD'
+  });
+  
 
 
 // Export model
